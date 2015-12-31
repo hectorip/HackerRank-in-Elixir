22d7ded7616ab8bf4b221ffa4ae38d69a36334d6
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-12-30 23:59:59 -0600

    Plus minus Resolved

--- plus-minus.exs
+++ plus-minus.exs
@@ -4,10 +4,25 @@ defmodule PlusMinus do
     n = IO.gets "Number of elements"
     list = IO.gets "List of Elements"
     list = String.split(list, " ") |> Enum.map fn el -> {i, _} = Integer.parse(el); i end
-    solution(n, list, {})
+    IO.inspect list
+    {n, _} = Integer.parse(n)
+    solution(n, list, {0, 0, 0})
   end
-
-  def solution(n, [i | tail], acc ) do
-    IO.inspect tail
+  
+  def solution(0, _, { pos, neg, zero }) do
+    total = pos + neg + zero
+    IO.puts pos/total
+    IO.puts neg/total
+    IO.puts zero/total
+  end
+  def solution(n, [i | tail], { pos, neg, zero } ) do
+    cond do
+      i > 0 ->
+        solution(n-1, tail, {pos + 1, neg, zero})
+      i < 0 ->
+        solution(n-1, tail, {pos, neg + 1, zero})
+      true ->
+        solution(n-1, tail, {pos, neg, zero + 1})
+    end
   end
 end
