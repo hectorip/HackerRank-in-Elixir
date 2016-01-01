4c3be5e619a7f75953c8caa7d2a3dfec88997473
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-01-01 00:21:09 -0600

    some style and comments to remind my future self what was I thinking of

--- plus-minus.exs
+++ plus-minus.exs
@@ -1,13 +1,14 @@
 defmodule PlusMinus do
   @doc "Problem description: https://www.hackerrank.com/challenges/plus-minus"
   def get_data do
-    n = IO.gets "Number of elements"
-    list = IO.gets "List of Elements"
+    n = IO.gets "Number of elements: "
+    list = IO.gets "List of Elements: "
     list = String.split(list, " ") |> Enum.map fn el -> {i, _} = Integer.parse(el); i end
     IO.inspect list
     {n, _} = Integer.parse(n)
     solution(n, list, {0, 0, 0})
   end
+  # avoid division by Zero
   def solution(0, _, {pos, neg, zero}) when (pos + neg + zero) == 0 do
     IO.puts 0
     IO.puts 0
@@ -19,6 +20,7 @@ defmodule PlusMinus do
     IO.puts neg/total
     IO.puts zero/total
   end
+  # Is this better with guards?
   def solution(n, [i | tail], { pos, neg, zero } ) do
     cond do
       i > 0 ->
